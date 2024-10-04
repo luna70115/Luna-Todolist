6d1a5c28bb5379f6ae7f31cc53ebb0df39be3b8a
BaGuai8 <vuxups70115@gmail.com>
2024-10-04 16:12:29 +0800

    button 元件修改

--- src/ui/button/button.jsx
+++ src/ui/button/button.jsx
@@ -1,11 +1,7 @@
 import "./style.scss";
 export function Button({ isLoading, children, className = "", ...rest }) {
   return (
-    <button
-      {...rest}
-      className={`button ${className} ${isLoading && `button__loading`}`}
-      disabled={isLoading}
-    >
+    <button {...rest} className={`button ${className}`} disabled={isLoading}>
       {children}
     </button>
   );
