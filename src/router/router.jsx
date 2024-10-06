88ac026bd498059cc9e9d55b376791efc15afc72
BaGuai8 <vuxups70115@gmail.com>
2024-10-06 16:27:33 +0800

    小修改

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,9 +1,9 @@
-import { createHashRouter } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import { Register } from "../routes/register/register";
 import { Login } from "../routes/login/login";
 import { Todolist } from "../routes/todo-list/todo-list";
 import { Error } from "../routes/error/error";
-export const router = createHashRouter([
+export const router = createBrowserRouter([
   { path: "/", element: <Login /> },
   { path: "register", element: <Register /> },
   { path: "todo-list", element: <Todolist /> },
